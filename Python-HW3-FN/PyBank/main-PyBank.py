4829931bca9a6da55a4e61b5a66eae223c238b52
fionampg <47542366+fionampg@users.noreply.github.com>
2019-03-30 23:46:32 -0400

    Add files via upload

---
+++ Python-HW3-FN/PyBank/main-PyBank.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 11:54:06 2019
+
+@author: nguye
+"""
+
+#import modules
+import os
+import csv
+
+# Path for the CSV file 
+
+file_csv = os.path.join("Resources","budget_data.csv")
+
+# Initiate export file
+exportfile = "finanalysis.txt"
+
+# Empty lists to store data. 
+profit = []
+monthlychange = []
+monthcount = []
+
+# Initialize the variables 
+totalmonthcount = 0
+startprofit = 0
+totalprofit = 0
+totchangeprofit = 0
+
+
+# Open the CSV using the set path 
+
+with open(file_csv, 'r') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csv_reader)
+
+    for row in csv_reader:    
+      # Count the number months in this dataset
+      totalmonthcount = totalmonthcount + 1 
+      
+      # Add profit to list, calculate the total profit
+      profit.append(row[1])
+      totalprofit = totalprofit + int(row[1])
+
+      # Calculate the avg change in profits m/m, calulate the avg change in profits
+      finalprofit = int(row[1])
+      monthlychangeprofits = finalprofit - startprofit
+
+      # Add monthly changes in a list
+      monthlychange.append(monthlychangeprofits)
+
+      totchangeprofit = totchangeprofit + monthlychangeprofits
+      startprofit = finalprofit
+
+      # Calculate the avg change in profits
+      avg_change_profit = (totchangeprofit/totalmonthcount)
+      
+      # Collect the largest increase/decrease in profits
+      monthcount.append(row[0])      
+      
+      # Look for the max/min change in profits with dates
+      maxprofit = max(monthlychange)
+      minprofit = min(monthlychange)
+
+      maxprofitdate = monthcount[monthlychange.index(maxprofit)]
+      minprofitdate = monthcount[monthlychange.index(minprofit)]
+    
+    #Print results
+    print("Financial Analysis")
+    print(
+            f"--------------------\n"
+            f"Total Months: {totalmonthcount}\n"
+            f"Total Profits: ${totalprofit}\n"
+            f"Average Change: $ {avg_change_profit}\n"
+            f"Greatest Increase in Profits: {maxprofitdate} (${maxprofit})\n"
+            f"Greatest Decrease in Profits: {minprofitdate} (${minprofit})\n"
+            )
+#Export results to new file
+#with open(exportfile, 'w') as file:
+ #   file.write("Financial Analysis\n")
+  #  file.write(
+   #         f"---------------------------\n"
+    #        f"Total Months: {totalmonthcount)\n"
+     #       f"Total Profits: {totalprofit}\n"
+      #      f"Average Change: ${avg_change_profit}\n"
+       #     f"Greatest Increase in Profits: {maxprofitdate} (${maxprofit})\n"
+        #    f"Greatest Decrease in Profits: {minprofitdate} (${minprofit})\n"
+         #   )
